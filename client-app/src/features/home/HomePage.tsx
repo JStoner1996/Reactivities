07f9d8d1b297c126c3f3b861c104f89ebe35a205
Jake Stoner <jakestoner123@live.com>
2025-01-16 12:25:11 +0000

    --feat: moved home page outside of navbar

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 const HomePage: React.FC = () => {
   return (
     <Container style={{ marginTop: "7em" }}>
       <h1>Home Page</h1>
+      <h3>
+        Go to <Link to="/activities">Activities</Link>
+      </h3>
     </Container>
   );
 };
