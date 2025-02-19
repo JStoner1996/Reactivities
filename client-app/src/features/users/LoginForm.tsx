736d8bd4866b9ee1df7ccfef8616c0e32a55eeca
Jake Stoner <jakestoner123@live.com>
2025-02-19 12:22:28 +0000

    --feat: added redirection after login and show error messages

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import TextInput from "../../app/common/form/TextInput";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
 
@@ -9,13 +9,30 @@ const LoginForm: React.FC = () => {
 
   return (
     <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => userStore.login(values)}
+      initialValues={{ email: "", password: "", error: null }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .login(values)
+          .catch(() => setErrors({ error: "Invalid email or password" }))
+      }
     >
-      {({ handleSubmit, isSubmitting }) => (
+      {({ handleSubmit, isSubmitting, errors }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
           <TextInput placeholder="email" name="email" />
+          <>bob@test.com - FOR TESTING PURPOSES</>
           <TextInput placeholder="password" name="password" type="password" />
+          <>Pa$$w0rd - FOR TESTING PURPOSES</>
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label
+                style={{ marginBottom: 10 }}
+                basic
+                color="red"
+                content={errors.error}
+              />
+            )}
+          />
           <Button
             loading={isSubmitting}
             positive
